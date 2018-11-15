f9e971c898dcc15fa18a13a184359526c5de5cca
jfbischoff <bischoff@vsp.tu-berlin.de>
2018-11-15 16:43:42 +0100

    adding a 0.01 model

--- scenario/se_14.0.1/output/readme.md
+++ scenario/se_14.0.1/output/readme.md
@@ -1,3 +1,4 @@
 Scenario output data for this scenario can be found under
 
 <http://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/se/matsim-sweden/se_14.0.1/>
+
--- src/main/java/org/matsim/vsp/ers/demand/DownscaleDemand.java
+++ src/main/java/org/matsim/vsp/ers/demand/DownscaleDemand.java
@@ -17,46 +17,28 @@
  *                                                                         *
  * *********************************************************************** */
 
-package org.matsim.vsp.ers;/*
- * created by jbischoff, 06.06.2018
+package org.matsim.vsp.ers.demand;/*
+ * created by jbischoff, 15.11.2018
  */
 
-import org.matsim.api.core.v01.Scenario;
-import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.controler.AbstractModule;
-import org.matsim.core.controler.Controler;
+import org.matsim.core.population.io.StreamingPopulationReader;
+import org.matsim.core.population.io.StreamingPopulationWriter;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.pt.transitSchedule.api.TransitSchedule;
-import org.matsim.vsp.ers.ptrouting.SwissRailRaptorModuleAsTeleportedMode;
-import org.matsim.vsp.ers.scoring.AgentSpecificASCScoring;
 
-public class RunSweden {
+public class DownscaleDemand {
 
     public static void main(String[] args) {
+        String inputDemand = "C:/Users/Joschka/git/matsim-sweden/scenario/se_14.0.1/input/merged_population.xml.gz";
 
-        String configFile = "scenario/se_14.0.01/input/config_0.01.xml";
+        String outputDemand = "C:/Users/Joschka/git/matsim-sweden/scenario/se_14.0.1/input/merged_population_0.01.xml.gz";
+        double sample = 0.1;
+        StreamingPopulationWriter spw = new StreamingPopulationWriter(sample);
+        StreamingPopulationReader spr = new StreamingPopulationReader(ScenarioUtils.createScenario(ConfigUtils.createConfig()));
+        spw.startStreaming(outputDemand);
+        spr.addAlgorithm(spw);
+        spr.readFile(inputDemand);
+        spw.closeStreaming();
 
-
-        //for running on  a server
-        if (args.length > 0) {
-            configFile = args[0];
-        }
-        Config config = ConfigUtils.loadConfig(configFile);
-        config.transit().setUseTransit(false);
-        Scenario scenario = ScenarioUtils.loadScenario(config);
-
-        Controler controler = new Controler(scenario);
-        controler.addOverridingModule(new AbstractModule() {
-            @Override
-            public void install() {
-                bindScoringFunctionFactory().to(AgentSpecificASCScoring.class);
-                bind(TransitSchedule.class).toInstance(scenario.getTransitSchedule());
-            }
-        });
-        controler.addOverridingModule(new SwissRailRaptorModuleAsTeleportedMode());
-
-        controler.run();
     }
-
 }
